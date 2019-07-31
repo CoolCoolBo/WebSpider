f573df8ed38e3a398c3f666c9880a73ab923305f
CoolCoolBo <512167784@qq.com>
2019-07-31 19:58:54 +0800

    first commit

--- 3_正则.py
+++ 3_正则.py
@@ -10,6 +10,7 @@
 # print(result.group())
 # print(result.span())
 
+
 # 匹配目标
 # import re
 
@@ -20,6 +21,7 @@
 # print(result.group(1))
 # print(result.span())
 
+
 # 通用匹配
 # import re
 
@@ -28,6 +30,7 @@
 # print(result)
 # print(result.group())
 # print(result.span())
+
 
 # 贪婪和非贪婪
 # import re
@@ -38,6 +41,7 @@
 # print(result.group(1))
 # print(result.span())
 
+
 # 修饰符
 # import re
 
@@ -46,6 +50,7 @@
 # '''
 # result = re.match('^He.*?(\d+).*?Demo$', content, re.S)
 # print(result.group(1))
+
 
 # 转义匹配
 # import re
@@ -88,26 +93,48 @@
 # result = re.search('<li.*?active.*?singer="(.*?)">(.*?)</a>', html, re.S)
 # print(result.group(1), result.group(2))
 
+
 # -------------------------- findall()--------------------------------
+# import re
+
+# html = '''< div id="songs-list" >
+# <h2 class = "title">经典老歌</h2>
+# <p class="introduction">
+# 经典老歌列表
+# </p>
+# <ul id="list" class="list-group">
+# <li data-view="2">一路上有你</li>
+# <li data-view="7">
+# <a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
+# </li>
+# <li data-view="4" class="active">
+# <a href="/3.mp3" singer="齐秦">往事随风</a>
+# </li>
+# <li data-view="6" class="active"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
+# <li data-view="5" class="active"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
+# <li data-view="5" class="active"><a href="/6.mp3" singer="邓丽君">但愿人长久</a></li>
+# </ul>
+# </div>'''
+# results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>', html, re.S)
+# print(results)
+
+
+# -------------------------- sub()--------------------------------
+# import re
+
+# content = 'adfa2542adf22asf2141kasf3s3g5'
+# content = re.sub('\d+', '', content)
+# print(content)
+
+
+# -------------------------- sub()--------------------------------
 import re
 
-html = '''< div id="songs-list" >
-<h2 class = "title">经典老歌</h2>
-<p class="introduction">
-经典老歌列表
-</p>
-<ul id="list" class="list-group">
-<li data-view="2">一路上有你</li>
-<li data-view="7">
-<a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
-</li>
-<li data-view="4" class="active">
-<a href="/3.mp3" singer="齐秦">往事随风</a>
-</li>
-<li data-view="6" class="active"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
-<li data-view="5" class="active"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
-<li data-view="5" class="active"><a href="/6.mp3" singer="邓丽君">但愿人长久</a></li>
-</ul>
-</div>'''
-results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>', html, re.S)
-print(results)
+content1 = '2016-12-15 12:00'
+content2 = '2016-12-17 12:55'
+content3 = '2016-12-17 13:55'
+pattern = re.compile('\d{2}:\d{2}')
+result1 = re.sub(pattern, '', content1)
+result2 = re.sub(pattern, '', content2)
+result3 = re.sub(pattern, '', content3)
+print(result1, result2, result3)
